3c89edded0d6bd3bffbb3736fb6a6ec6667e8a98
Jacob Banducci <jbanducci321@gmail.com>
2026-02-11 11:34:59 -0800

    Finished the first part of lab 2, need to finalize the partner code portion

--- guess_number/js/script.js
+++ guess_number/js/script.js
@@ -64,7 +64,7 @@ function checkGuess() {
     else {
         document.querySelector("#guesses").textContent += guess + " ";
         if (attempts == 7) {
-            feedback.textContent = "Sorry you lost";
+            feedback.textContent = "Sorry you lost. The number was " + randomNumber;
             feedback.style.color = "red";
             totalLosses++;
             document.querySelector("#totalLosses").textContent = totalLosses;
