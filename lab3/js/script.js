9781e8c9d889d8cb3c3bb92530aad82c91b60250
Jacob Banducci <jbanducci321@gmail.com>
2026-02-20 19:46:54 -0800

    Finished the main parts of lab 3 Need to do all the CSS for it now

--- lab3/js/script.js
+++ lab3/js/script.js
@@ -1,8 +1,14 @@
 //Event listeners
-document.querySelector("button").addEventListener("click", submitQuiz)
+document.querySelector("#submitButton").addEventListener("click", submitQuiz);
+document.querySelector("#submitButton").addEventListener("click", quizCounter);
 
 //Global variables
 let score = 0;
+
+
+document.getElementById("numAttempts").innerHTML = localStorage.clickcount;
+
+document.querySelector("#congratsDisplay").style.display = "none";
 
 shuffleQ1Choices();
 function shuffleQ1Choices() {
@@ -30,66 +36,144 @@ function shuffleQ1Choices() {
 }
 
 function submitQuiz() {
-    let radioVal = document.querySelector("input[name='q1']:checked").value;
+
+    score = 0; //Resets the score for every submit
+
+    let radioChecked = document.querySelector("input[name='q1']:checked");
     let textVal = document.querySelector("#q2").value;
     let numberVal = document.querySelector("#q3").value;
     let selectVal = document.querySelector("#q4").value;
 
-    //For question 1
-    if (radioVal == "Rhydon") {
+    //clears any set images
+    document.querySelector("#q1img").innerHTML = "";
+    document.querySelector("#q2img").innerHTML = "";
+    document.querySelector("#q3img").innerHTML = "";
+    document.querySelector("#q4img").innerHTML = "";
+    document.querySelector("#q5img").innerHTML = "";
+
+    //q1
+    if (radioChecked && radioChecked.value === "Rhydon") {
+
         document.querySelector("#q1Feedback").textContent = "Correct Answer";
         document.querySelector("#q1Feedback").style.color = "green";
         score += 20;
-    }
-    else {
+
+        let img1 = document.createElement("img");
+        img1.src = "./img/correct.png";
+        document.querySelector("#q1img").append(img1);
+
+    } else {
+
         document.querySelector("#q1Feedback").textContent = "Wrong Answer";
         document.querySelector("#q1Feedback").style.color = "red";
+
+        let img1 = document.createElement("img");
+        img1.src = "./img/x_mark.png";
+        document.querySelector("#q1img").append(img1);
     }
 
-    //For question 2
-    if (textVal == "Mewtwo" || textVal == "mewtwo") {
+    //q2
+    if (textVal === "Mewtwo" || textVal === "mewtwo") {
+
         document.querySelector("#q2Feedback").textContent = "Correct Answer";
         document.querySelector("#q2Feedback").style.color = "green";
         score += 20;
-    }
-    else {
+
+        let img2 = document.createElement("img");
+        img2.src = "./img/correct.png";
+        document.querySelector("#q2img").append(img2);
+
+    } else {
+
         document.querySelector("#q2Feedback").textContent = "Wrong Answer";
         document.querySelector("#q2Feedback").style.color = "red";
+
+        let img2 = document.createElement("img");
+        img2.src = "./img/x_mark.png";
+        document.querySelector("#q2img").append(img2);
     }
 
-    //For question 3
-    if (numberVal == "1996") {
+    //q3
+    if (numberVal === "1996") {
+
         document.querySelector("#q3Feedback").textContent = "Correct Answer";
         document.querySelector("#q3Feedback").style.color = "green";
         score += 20;
-    }
-    else {
+
+        let img3 = document.createElement("img");
+        img3.src = "./img/correct.png";
+        document.querySelector("#q3img").append(img3);
+
+    } else {
+
         document.querySelector("#q3Feedback").textContent = "Wrong Answer";
         document.querySelector("#q3Feedback").style.color = "red";
+
+        let img3 = document.createElement("img");
+        img3.src = "./img/x_mark.png";
+        document.querySelector("#q3img").append(img3);
     }
 
-    //For question 4
-    if (selectVal == "Electric") {
+    //q4
+    if (selectVal === "Electric") {
+
         document.querySelector("#q4Feedback").textContent = "Correct Answer";
         document.querySelector("#q4Feedback").style.color = "green";
         score += 20;
-    }
-    else {
+
+        let img4 = document.createElement("img");
+        img4.src = "./img/correct.png";
+        document.querySelector("#q4img").append(img4);
+
+    } else {
+
         document.querySelector("#q4Feedback").textContent = "Wrong Answer";
         document.querySelector("#q4Feedback").style.color = "red";
+
+        let img4 = document.createElement("img");
+        img4.src = "./img/x_mark.png";
+        document.querySelector("#q4img").append(img4);
     }
 
-    //For question 5
-    if (document.querySelector("#q5_1").checked || document.querySelector("#q5_2").checked) {
+    //q5
+    if (document.querySelector("#q5_1").checked &&
+        document.querySelector("#q5_2").checked) {
+
         document.querySelector("#q5Feedback").textContent = "Correct Answer";
         document.querySelector("#q5Feedback").style.color = "green";
         score += 20;
-    }
-    else {
+
+        let img5 = document.createElement("img");
+        img5.src = "./img/correct.png";
+        document.querySelector("#q5img").append(img5);
+
+    } else {
+
         document.querySelector("#q5Feedback").textContent = "Wrong Answer";
         document.querySelector("#q5Feedback").style.color = "red";
+
+        let img5 = document.createElement("img");
+        img5.src = "./img/x_mark.png";
+        document.querySelector("#q5img").append(img5);
     }
 
-    //Score stuff
+    //Display total score
     document.querySelector("#scoreDisplay").textContent = "Total score: " + score;
+
+    if (score >= 80) {
+        document.querySelector("#congratsDisplay").textContent =
+            "Congratulations! You scored 80 or higher!";
+        document.querySelector("#congratsDisplay").style.color = "blue";
+    } else {
+        document.querySelector("#congratsDisplay").textContent = "";
+    }
+}
+
+function quizCounter() {
+    if (localStorage.clickcount) {
+        localStorage.clickcount = Number(localStorage.clickcount)+1;
+    } else {
+        localStorage.clickcount = 1;
+    }
+    document.querySelector("#numAttempts").innerHTML = localStorage.clickcount;
 }
